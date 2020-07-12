09b736bac5d6194507400ef227dbd9f43c0009d2
Felipe Vieira <mrchampz@outlook.com>
2020-07-11 21:10:52 -0300

    Build de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api/v1'
+  apiUrl: 'http://localhost:5000/api/v1',
+  tinygraphsUrl: 'https://www.tinygraphs.com/labs/squares/random?theme=berrypie&numcolors=4&size=256&fmt=svg'
 };
