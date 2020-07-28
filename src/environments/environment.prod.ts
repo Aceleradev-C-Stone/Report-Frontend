663cf256f70e298b70a241707b3b60a691cc92c1
Felipe Vieira <mrchampz@outlook.com>
2020-07-27 21:30:07 -0300

    Correção da url base de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://report-aceleradev.azurewebsites.net/',
+  apiUrl: 'https://report-aceleradev.azurewebsites.net/api/v1',
   tinygraphsUrl: 'https://www.tinygraphs.com/labs/squares/random?theme=berrypie&numcolors=4&size=256&fmt=svg'
 };
