4c6cdb11171bda58596c777c2713e711e901dda1
Felipe Vieira <mrchampz@outlook.com>
2020-07-27 21:35:37 -0300

    Adicionada a url do github às variáveis de ambiente de desenvolvimento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api/v1',
-  tinygraphsUrl: 'https://www.tinygraphs.com/labs/squares/random?theme=berrypie&numcolors=4&size=256&fmt=svg'
+  tinygraphsUrl: 'https://www.tinygraphs.com/labs/squares/random?theme=berrypie&numcolors=4&size=256&fmt=svg',
+  githubUrl: 'https://github.com/MrChampz'
 };
 
 /*
